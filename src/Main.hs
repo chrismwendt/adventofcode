555bc6316c68bc65f3cf86d69284abc78262a7cc
Chris Wendt <chrismwendt@gmail.com>
2018-11-30 21:46:48 -0800

    init

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,23 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-main :: IO ()
-main = do
-  putStr "hello, world\n"
+import qualified Data.Set as Set
+import Data.Maybe
 
-  return ()
+main :: IO ()
+main = day1part2
 
 ghci :: IO ()
 ghci = main
+
+day1part1 :: IO ()
+day1part1 = do
+  input <- readFile "input1.txt"
+  print $ sum $ map (read . filter (/= '+')) $ lines input
+
+day1part2 :: IO ()
+day1part2 = do
+  input <- readFile "input1.txt"
+  let
+    frequencies = scanl (+) 0 $ map (read . filter (/= '+')) $ cycle $ lines input :: [Int]
+    dupes set (freq : rest) = (if Set.member freq set then [freq] else []) ++ dupes (Set.insert freq set) rest
+  print $ head $ dupes Set.empty frequencies
