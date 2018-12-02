01f206104a392dcd7e3d6b9dc33eb8df310aaa11
Chris Wendt <chrismwendt@gmail.com>
2018-12-02 12:47:37 -0800

    day2

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE TupleSections #-}
+
 module Main where
 
 import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Data.List.Index
+import Control.Monad.Loops
 
 ghci :: IO ()
 ghci = main
@@ -8,16 +13,39 @@ ghci = main
 main :: IO ()
 main = do
   day1
+  day2
+
+firstDupe :: Ord a => [a] -> Maybe a
+firstDupe = go Set.empty
+  where
+  go _ [] = Nothing
+  go set (a : rest)
+    | Set.member a set = Just a
+    | otherwise = go (Set.insert a set) rest
 
 day1 :: IO ()
 day1 = do
   input <- readFile "input1.txt"
+
   let
     changes = map (read . filter (/= '+')) $ lines input :: [Int]
     frequencies = scanl (+) 0
-    firstDupe set (freq : rest)
-      | Set.member freq set = freq
-      | otherwise = firstDupe (Set.insert freq set) rest
 
-  print $ sum changes
-  print $ firstDupe Set.empty (frequencies (cycle changes))
+  putStrLn $ "1a: " ++ show (sum changes)
+  putStrLn $ "1b: " ++ maybe "BUG" show (firstDupe (frequencies (cycle changes)))
+
+day2 :: IO ()
+day2 = do
+  input <- readFile "input2.txt"
+
+  let
+    count p = length . filter p
+    ns n = count ((n `elem`) . Map.elems . Map.fromListWith (+) . map (, 1)) $ lines input
+    checksum = ns 2 * ns 3
+
+  putStrLn $ "2a: " ++ show checksum
+
+  let
+    dropEachLetter s = imap (\i _ -> (i, deleteAt i s)) s
+
+  putStrLn $ "2b: " ++ maybe "BUG" snd (firstDupe $ concat $ map dropEachLetter $ lines input)
