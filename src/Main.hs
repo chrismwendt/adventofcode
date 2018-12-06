ce9d69fbcf0bd953b6776665a30207796de7aa3c
Chris Wendt <chrismwendt@gmail.com>
2018-12-06 00:19:59 -0800

    day3a

--- src/Main.hs
+++ src/Main.hs
@@ -5,15 +5,20 @@ module Main where
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 import Data.List.Index
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
+import Data.Void
 import Control.Monad.Loops
+import Linear.V2
+import Data.Array
 
 ghci :: IO ()
 ghci = main
 
 main :: IO ()
 main = do
-  day1
-  day2
+  day3
 
 firstDupe :: Ord a => [a] -> Maybe a
 firstDupe = go Set.empty
@@ -49,3 +54,31 @@ day2 = do
     dropEachLetter s = imap (\i _ -> (i, deleteAt i s)) s
 
   putStrLn $ "2b: " ++ maybe "BUG" snd (firstDupe $ concat $ map dropEachLetter $ lines input)
+
+day3 :: IO ()
+day3 = do
+  input <- readFile "input3.txt"
+
+  let
+    int = fromIntegral <$> decimal
+
+    lineParser :: Parsec Void String (Int, Int, Int, Int)
+    lineParser = do
+      char '#' >> int >> string " @ "
+      (left, top) <- (,) <$> decimal <* char ',' <*> decimal
+      string ": "
+      (width, height) <- (,) <$> decimal <* char 'x' <*> decimal
+      char '\n'
+      return (left, top, width, height)
+
+    claimToCells (left, top, width, height) =
+      [ V2 (left + a) (top + b)
+      | a <- [0 .. width - 1]
+      , b <- [0 .. height - 1]
+      ]
+
+    size = (V2 0 0, V2 1000 1000)
+    stack claims = accumArray (+) 0 size $ map (, 1) $ (concatMap claimToCells) claims
+    overlap claims = sum $ fmap (\depth -> if depth >= 2 then 1 else 0) $ stack claims
+
+  putStrLn $ "3a: " ++ maybe "BUG" (show . overlap) (parseMaybe (many lineParser) input)
