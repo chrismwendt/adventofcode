c679d67f1f638919a0b2353dcdd2d6ad582342e7
Chris Wendt <chrismwendt@gmail.com>
2018-12-08 01:08:18 -0800

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 import qualified Data.Set as Set
 import qualified Data.Map as Map
+import Data.List
 import Data.List.Index
 import Text.Megaparsec
 import Text.Megaparsec.Char
@@ -13,6 +14,8 @@ import Control.Monad.Loops
 import Linear.V2
 import Data.Array
 import Safe
+import Data.Ord
+import Data.Tuple.Extra
 import Data.Foldable
 
 ghci :: IO ()
@@ -20,7 +23,7 @@ ghci = main
 
 main :: IO ()
 main = do
-  day3
+  day4
 
 firstDupe :: Ord a => [a] -> Maybe a
 firstDupe = go Set.empty
@@ -90,3 +93,65 @@ day3 = do
 
   putStrLn $ "3a: " ++ maybe "BUG" (show . measureOverlap) (parseMaybe (many claimParser) input)
   putStrLn $ "3b: " ++ maybe "BUG" show (parseMaybe (many claimParser) input >>= findNonOverlapping)
+
+data Time = Time Int Int Int Int Int deriving (Eq, Ord, Show)
+data Log = Begin Int | Sleep | Wake deriving (Eq, Ord, Show)
+type Minute = Int
+
+day4 :: IO ()
+day4 = do
+  input <- readFile "input4.txt"
+
+  let
+    int = fromIntegral <$> decimal
+    logParser :: Parsec Void String (Time, Log)
+    logParser = do
+      char '['
+      y <- int <* char '-'
+      mo <- int <* char '-'
+      d <- int <* char ' '
+      h <- int <* char ':'
+      mi <- int
+      char ']'
+      char ' '
+      l <- choice
+        [ Begin <$> (string "Guard #" *> int <* string " begins shift")
+        , Sleep <$ string "falls asleep"
+        , Wake <$ string "wakes up"
+        ]
+      char '\n'
+      return $ (Time y mo d h mi, l)
+
+    minute (Time _ _ _ _ m) = m
+    frequencies = Map.fromListWith (+) . map (, 1)
+    range a b = [a .. b - 1]
+    keyOfMax = fmap fst . maximumByMay (comparing snd) . Map.toList
+    minuteMode xs = keyOfMax $ frequencies $ concatMap (uncurry range) xs
+    totalSleep = sum . map (\(a, b) -> b - a)
+    groupByKey = Map.fromListWith (++) . map (\(a, b) -> (a, [b]))
+    minuteOnly = map (\(t, g) -> (minute t, g))
+    guardNaps :: [(Minute, Log)] -> [(Int, (Minute, Minute))]
+    guardNaps xs =
+      let
+        go _ [] = []
+        go _ ((t, Begin g) : rest) = go (Just (t, g)) rest
+        go Nothing _ = [] -- error: this means the log did not start with Begin
+        go (Just (_, g)) ((t, Sleep) : rest) = go (Just (t, g)) rest
+        go (Just (t, g)) ((t2, Wake) : rest) = (g, (t, t2)) : go (Just (t2, g)) rest
+      in
+        go Nothing xs
+    partA logs = do
+      (guard, naps) <- maximumByMay (comparing $ totalSleep . snd)
+        $ Map.toList $ groupByKey $ guardNaps $ minuteOnly $ sort logs
+      minute <- minuteMode naps
+      return $ guard * minute
+    partB logs = do
+      maxGuardDurationByMinute <- mapM (\(a, b) -> (a, ) <$> maximumByMay (comparing snd) b)
+        $ Map.toList $ fmap Map.toList $ fmap frequencies $ groupByKey
+        $ concatMap ((\(g, ts) -> map (, g) ts) . second (uncurry range))
+        $ guardNaps $ minuteOnly $ sort logs
+      (minute, (guard, _)) <- maximumByMay (comparing (snd . snd)) maxGuardDurationByMinute
+      return $ guard * minute
+
+  putStrLn $ "4a: " ++ maybe "BUG" show (parseMaybe (many logParser) input >>= partA)
+  putStrLn $ "4a: " ++ maybe "BUG" show (parseMaybe (many logParser) input >>= partB)
