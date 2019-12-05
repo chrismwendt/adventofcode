932cb88652ec7e073b93bf7d23696d42e988039e
Chris Wendt <chrismwendt@gmail.com>
2019-12-05 12:03:18 -0700

    2019

--- src/Main.hs
+++ src/Main.hs
@@ -1,159 +1,105 @@
 {-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Main where
 
-import qualified Data.Set as Set
-import qualified Data.Map as Map
+import Control.Monad.Loops
 import Data.List
 import Data.List.Index
-import Text.Megaparsec
-import Text.Megaparsec.Char
-import Text.Megaparsec.Char.Lexer
-import Data.Void
-import Control.Monad.Loops
-import Linear.V2
-import Data.Array
-import Safe (maximumMay, maximumByMay, maximumDef, headMay)
-import Data.Ord
-import Data.Tuple.Extra
-import Control.Monad.Trans.State.Lazy
-import Lens.Micro.Platform
-import Data.Foldable
+import Data.List.Split
+import qualified Data.Map as Map
+import Data.Maybe
+import qualified Data.Set as Set
 
 ghci :: IO ()
 ghci = main
 
 main :: IO ()
 main = do
-  day4
-
-firstDupe :: Ord a => [a] -> Maybe a
-firstDupe = go Set.empty
-  where
-  go _ [] = Nothing
-  go set (a : rest)
-    | Set.member a set = Just a
-    | otherwise = go (Set.insert a set) rest
+  day5
 
 day1 :: IO ()
 day1 = do
   input <- readFile "input1.txt"
-
-  let
-    changes = map (read . filter (/= '+')) $ lines input :: [Int]
-    frequencies = scanl (+) 0
-
-  putStrLn $ "1a: " ++ show (sum changes)
-  putStrLn $ "1b: " ++ maybe "BUG" show (firstDupe (frequencies (cycle changes)))
+  let fuel modul = modul `div` 3 - 2
+  let allfuel modul = sum $ unfoldr (\fuel -> if fuel <= 0 then Nothing else Just (fuel, fuel `div` 3 - 2)) (fuel modul)
+  putStrLn $ "1a: " ++ show (sum $ map (fuel . read) $ lines input)
+  putStrLn $ "1b: " ++ show (sum $ map (allfuel . read) $ lines input)
 
 day2 :: IO ()
 day2 = do
   input <- readFile "input2.txt"
-
-  let
-    count p = length . filter p
-    frequencies = map (Map.elems . Map.fromListWith (+) . map (, 1)) $ lines input
-    checksum = count (2 `elem`) frequencies * count (3 `elem`) frequencies
-
-  putStrLn $ "2a: " ++ show checksum
-
-  let
-    dropEachLetter s = imap (\i _ -> (i, deleteAt i s)) s
-
-  putStrLn $ "2b: " ++ maybe "BUG" snd (firstDupe $ concat $ map dropEachLetter $ lines input)
+  let prog = Map.fromList $ zip [0 ..] (map (read :: String -> Int) $ splitOn "," input)
+  let halt = Map.insert 1 12 . Map.insert 2 2 :: Map.Map Int Int -> Map.Map Int Int
+  let set noun verb = Map.insert 1 noun . Map.insert 2 verb :: Map.Map Int Int -> Map.Map Int Int
+  let run i p =
+        let at = (p Map.!)
+         in case at i of
+              1 -> run (i + 4) (Map.insert (at (i + 3)) (at (at (i + 1)) + at (at (i + 2))) p)
+              2 -> run (i + 4) (Map.insert (at (i + 3)) (at (at (i + 1)) * at (at (i + 2))) p)
+              99 -> p
+  putStrLn $ "1a: " ++ show ((Map.! 0) $ run 0 $ halt prog)
+  let allnvs = [(noun, verb) | noun <- [0 .. 99], verb <- [0 .. 99]]
+  let needle (noun, verb) = (== 19690720) $ (Map.! 0) $ run 0 $ set noun verb prog
+  putStrLn $ "1b: " ++ show (let (noun, verb) = fromJust $ find needle allnvs in 100 * noun + verb)
 
 day3 :: IO ()
 day3 = do
   input <- readFile "input3.txt"
-
-  let
-    int = fromIntegral <$> decimal
-
-    claimParser :: Parsec Void String (Int, Int, Int, Int, Int)
-    claimParser = do
-      id <- char '#' *> int <* string " @ "
-      (left, top) <- (,) <$> decimal <* char ',' <*> decimal
-      string ": "
-      (width, height) <- (,) <$> decimal <* char 'x' <*> decimal
-      char '\n'
-      return (id, left, top, width, height)
-
-    claimToCells (id, left, top, width, height) =
-      [ (V2 (left + a) (top + b), [id])
-      | a <- [0 .. width - 1]
-      , b <- [0 .. height - 1]
-      ]
-
-    size = (V2 0 0, V2 1000 1000)
-    stack claims = accumArray (++) [] size $ (concatMap claimToCells) claims
-    measureOverlap claims = sum $ fmap (\ids -> if length ids >= 2 then 1 else 0) $ stack claims
-    findNonOverlapping claims =
-      headMay
-      $ Set.toList . foldl' (flip Set.delete) (Set.fromList (map (\(id, _, _, _, _) -> id) claims))
-      $ concat . filter (\ids -> length ids >= 2)
-      $ toList (stack claims)
-
-  putStrLn $ "3a: " ++ maybe "BUG" (show . measureOverlap) (parseMaybe (many claimParser) input)
-  putStrLn $ "3b: " ++ maybe "BUG" show (parseMaybe (many claimParser) input >>= findNonOverlapping)
-
-data Time = Time Int Int Int Int Int deriving (Eq, Ord, Show)
-data Log = Begin Int | Sleep | Wake deriving (Eq, Ord, Show)
-type Minute = Int
+  let dir :: Char -> (Int, Int)
+      dir 'U' = (0, 1)
+      dir 'D' = (0, -1)
+      dir 'L' = (-1, 0)
+      dir 'R' = (1, 0)
+      rd (c : val) = replicate (read val) (dir c)
+      d2w deltas = drop 1 $ scanl (\(x, y) (dx, dy) -> (x + dx, y + dy)) (0, 0) deltas
+      (w1 : w2 : _) = map (d2w . concatMap rd . splitOn ",") $ lines input
+      lay wire = Map.fromListWith min $ zip wire [1 ..]
+      manhattan (x, y) = abs x + abs y
+      intersections = Map.intersectionWith (+) (lay w1) (lay w2)
+  putStrLn $ "1a: " ++ show (minimum $ map manhattan (Map.keys intersections))
+  putStrLn $ "1b: " ++ show (minimum $ Map.elems intersections)
 
 day4 :: IO ()
 day4 = do
-  input <- readFile "input4.txt"
+  let input = "193651-649729"
+  let [lower, upper] = map read $ splitOn "-" input :: [Int]
+  let adjs xs = zip xs (tail xs)
+  let monotonic xs = all (\(a, b) -> a <= b) (adjs xs)
+  let meets n =
+        and
+          [ length (show n) == 6,
+            any ((>= 2) . length) (group (show n)),
+            monotonic (show n)
+          ]
+  let meets2 n =
+        and
+          [ length (show n) == 6,
+            any ((== 2) . length) (group (show n)),
+            monotonic (show n)
+          ]
+  putStrLn $ "1a: " ++ show (length $ filter meets [lower .. upper])
+  putStrLn $ "1b: " ++ show (length $ filter meets2 [lower .. upper])
 
-  let
-    int = fromIntegral <$> decimal
-    logParser :: Parsec Void String (Time, Log)
-    logParser = do
-      char '['
-      y <- int <* char '-'
-      mo <- int <* char '-'
-      d <- int <* char ' '
-      h <- int <* char ':'
-      mi <- int
-      char ']'
-      char ' '
-      l <- choice
-        [ Begin <$> (string "Guard #" *> int <* string " begins shift")
-        , Sleep <$ string "falls asleep"
-        , Wake <$ string "wakes up"
-        ]
-      char '\n'
-      return $ (Time y mo d h mi, l)
-
-    minute (Time _ _ _ _ m) = m
-    range a b = [a .. b - 1]
-    maxInMap = maximumByMay (comparing snd) . Map.toList
-    maxInMapBy f = maximumByMay (comparing (f . snd)) . Map.toList
-    maximumDef' f = maximumDef f . toList
-
-    flattenMaps :: (Ord k1, Ord k2) => Map.Map k1 (Map.Map k2 a) -> Map.Map (k1, k2) a
-    flattenMaps =
-      Map.fromList
-      . map (\(k1, (k2, v)) -> ((k1, k2), v))
-      . concatMap (\(k, kvs) -> map (k, ) kvs)
-      . Map.toList . fmap Map.toList
-
-    f (_, _, m) (_, Begin guard) = (guard, 0, m)
-    f (guard, _, m) (start, Sleep) = (guard, start, m)
-    f (guard, start, m) (stop, Wake) = (guard, 0, foldl' (flip add) m (range start stop))
-      where
-      add minute = at guard . non Map.empty . at minute . non 0 %~ (+ 1)
-
-    sleepByMinuteByGuard logs = thd3 $ foldl' f (0, 0, Map.empty) $ map (first minute) $ sort logs
-
-    partA logs = do
-      (g, sleepByMinute) <- maxInMapBy sum $ sleepByMinuteByGuard logs
-      (m, _) <- maxInMap sleepByMinute
-      return $ (g, m)
-
-    partB logs = do
-      ((g, m), _) <- maxInMap $ flattenMaps $ sleepByMinuteByGuard logs
-      return $ (g, m)
-
-  putStrLn $ "4a: " ++ maybe "BUG" show (parseMaybe (many logParser) input >>= partA)
-  putStrLn $ "4a: " ++ maybe "BUG" show (parseMaybe (many logParser) input >>= partB)
+day5 :: IO ()
+day5 = do
+  input <- readFile "input5.txt"
+  let prog = Map.fromList $ zip [0 ..] (map (read :: String -> Int) $ splitOn "," input)
+  let run input i p =
+        let at = (p Map.!)
+            digit num n = num `div` (10 ^ n) `mod` 10
+            get param = case at i `digit` (param - 1 + 2) of
+              0 -> at (at (i + param))
+              1 -> at (i + param)
+         in case at i `mod` 100 of
+              1 -> run input (i + 4) (Map.insert (at (i + 3)) (get 1 + get 2) p)
+              2 -> run input (i + 4) (Map.insert (at (i + 3)) (get 1 * get 2) p)
+              3 -> run (tail input) (i + 2) (Map.insert (at (i + 1)) (head input) p)
+              4 -> at (at (i + 1)) : run input (i + 2) p
+              5 -> run input (if get 1 /= 0 then get 2 else i + 3) p
+              6 -> run input (if get 1 == 0 then get 2 else i + 3) p
+              7 -> run input (i + 4) (Map.insert (at (i + 3)) (if get 1 < get 2 then 1 else 0) p)
+              8 -> run input (i + 4) (Map.insert (at (i + 3)) (if get 1 == get 2 then 1 else 0) p)
+              99 -> []
+              other -> error (show (at i))
+  putStrLn $ "1a: " ++ show (last $ run [1] 0 prog)
+  putStrLn $ "1b: " ++ show (head $ run [5] 0 prog)
